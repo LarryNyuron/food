9ea5d14548a7aa5d62ff15622705646a7b5ca626
LarryNyuron <mikhkravcoff@gmail.com>
2023-03-20 11:28:02 +0300

    Rewrite eda_pars on async rails

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import csv
+'''import csv
 import requests
 import aiohttp
 import asyncio
@@ -29,12 +29,9 @@ async def parse_page(session, url):
     soup = BeautifulSoup(html, 'html.parser')
     results = []
     for element in soup.find_all('a', 'emotion-12sjte8'):
-        item = {}
-        for key, value in SELECTORS.items():
-            selector = value['selector']
-            attr = value['attr']
-            elem = element.select_one(selector)
-            item[key] = elem.get(attr) if elem else None
+        item = []
+        for a in element:
+            item.append(a['href'] + ',n')
         results.append(item)
     return results
 
@@ -44,7 +41,7 @@ async def parse_all_pages():
     async with aiohttp.ClientSession() as session:
         tasks = []
         for i in range(1, NUM_PAGES + 1):
-            url = f'{BASE_URL}{i}'
+            url = f'BASE_URL}i}'
             tasks.append(asyncio.ensure_future(parse_page(session, url)))
         results = await asyncio.gather(*tasks)
     return [item for result in results for item in result]
@@ -56,3 +53,42 @@ if __name__ == '__main__':
     f = open('recipes.txt', 'a')
     f.write(items)
     f.close()
+
+
+
+Синхронно парсит
+import csv
+import requests
+import time
+import random
+import sys
+
+
+from additional_functions import what_page
+from bs4 import BeautifulSoup
+from func_part_of_recipe import recipe_obj
+
+
+base_url = 'https://eda.ru'
+url = 'https://eda.ru/recepty?page='
+i = '68'
+
+
+response = requests.get(url + i)
+
+
+#The cycle by which the pages of each individual recipe are pulled out
+f = open('recipes.txt', 'a')
+while response.status_code == 200:
+    print(response.status_code)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    all = soup.find_all('a', 'emotion-12sjte8')
+    for a in all:
+        f.write(a['href'] + ',\n')
+    what_page(i)
+    i = str(int(i) + 1)
+    response = requests.get(url + i)
+    time.sleep(random.randint(1, 5))
+f.close()
+
+'''
